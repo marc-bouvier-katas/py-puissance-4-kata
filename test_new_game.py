8dd379c02dee6600a244158d71bbe92ecfce7071
Marc Bouvier <m.bouvier.dev@gmail.com>
2019-06-29 23:28:20 +0200

    format code

--- test_new_game.py
+++ test_new_game.py
@@ -1,15 +1,17 @@
 from grid import *
 
+
 def test_new_game():
-    assert game().ascii() =="""OOOOOOO
+    assert Game().ascii() == """OOOOOOO
 OOOOOOO
 OOOOOOO
 OOOOOOO
 OOOOOOO
 OOOOOOO"""
 
+
 def test_play_red_in_first_column():
-    assert game().play('R',1).ascii() == """OOOOOOO
+    assert Game().play('R', 1).ascii() == """OOOOOOO
 OOOOOOO
 OOOOOOO
 OOOOOOO
